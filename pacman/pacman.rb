84d1e4ea9c307fde3089b737721f7b9c3f921176
Tomoyuki Matsuda <toocheap@gmail.com>
2010-08-21 02:01:26 +0900

    MOD: remove dot at initial position of pacman and enemyies

--- pacman/pacman.rb
+++ pacman/pacman.rb
@@ -1,45 +1,47 @@
+# coding:utf-8
+# vim:ts=2 sw=2 sts=0
 require 'pp'
 
 class Maze
-    class MazeObject
-        def initialize(x,y, character)
-            @x = x
-            @y = y
-            @ch = character
-        end
-        def to_c
-            @ch
-        end
-        def pos
-            [@x, @y]
-        end
+  class MazeObject
+    def initialize(x,y, character)
+      @x = x
+      @y = y
+      @ch = character
+    end
+    def to_c
+      @ch
+    end
+    def pos
+      [@x, @y]
+    end
+  end
+
+  class Enemy < MazeObject
+    def step(pcx, pcy)
+      compute(pcx, pcy)
     end
 
-    class Enemy < MazeObject
-        def step(pcx, pcy)
-            compute(pcx, pcy)
-        end
-
-        def compute(pcx, pcy)
-            # Each Enemy AI here. Compute which direction the enemy move to.
-        end
+    def compute(pcx, pcy)
+      # Each Enemy AI here. Compute which direction the enemy move to.
     end
+  end
 
-    class PacMan < MazeObject
-        # return vec
-        def go(direction)
-            case direction
-            when 'h'
-                [-1, 0]
-            when 'j'
-                [0, 1]
-            when 'k'
-                [0, -1]
-            when 'l'
-                [0, 1]
-            end
-        end
+  class PacMan < MazeObject
+    # return vec
+    def go(direction)
+      case direction
+      when 'h'
+        [-1, 0]
+      when 'j'
+        [0, 1]
+      when 'k'
+        [0, -1]
+      when 'l'
+        [0, 1]
+      end
     end
+  end
 
 =begin
 
@@ -51,228 +53,222 @@ Maze construct.
 
 TODO: Now all of enemys are :enemy.
 =end
-    def initialize(filename)
-        @objects = Array.new
+  def initialize(filename)
+    @objects = Array.new
 
-        File.open(filename) do |f|
-            @times = f.gets
-            @width, @height = f.gets.split
+    File.open(filename) do |f|
+      @times = f.gets
+      @width, @height = f.gets.split
 
-            @times = @times.to_i-1
-            @width = @width.to_i
-            @height = @height.to_i
-            @maze = Array.new(@height)
-            @crosses = Array.new
-            @roads = Array.new
+      @times = @times.to_i-1
+      @width = @width.to_i
+      @height = @height.to_i
+      @maze = Array.new(@height)
+      @crosses = Array.new
+      @roads = Array.new
 
-            h = 0
-            mz = []
-            while line = f.gets
-                mz[h] = line.chomp!.unpack("C*")
-                h+=1
-            end
-            _parse_maze(mz)
+      h = 0
+      mz = []
+      while line = f.gets
+        mz[h] = line.chomp!.unpack("C*")
+        h+=1
+      end
+      _parse_maze(mz)
+    end
+  end
+
+  def showmaze
+    puts "T:#{@times+1} W:#{@width} H:#{@height}"
+    maze = Array.new
+    @height.times do |y|
+      maze[y] = Array.new
+      @width.times do |x|
+#        puts "#{x},#{y}=#{@maze[y][x]}" if $DEBUG
+        case @maze[y][x]
+        when :wall
+          maze[y][x] = '#'
+        when :dot
+          maze[y][x] = '.'
         end
+      end
     end
-
-    def showmaze
-        puts "T:#{@times+1} W:#{@width} H:#{@height}"
-        maze = Array.new
-        @height.times do |y|
-            maze[y] = Array.new
-            @width.times do |x|
-#                puts "#{x},#{y}=#{@maze[y][x]}" if $DEBUG
-                case @maze[y][x]
-                when :wall
-                    maze[y][x] = '#'
-                when :dot
-                    maze[y][x] = '.'
-                end
-            end
-        end
-        @objects.each do |o|
-            ox, oy = o.pos
-            maze[oy][ox] = o.to_c
-        end
-        @height.times do |y|
-            @width.times do |x|
-                print maze[y][x]
-            end
-            puts
-        end
+    @objects.each do |o|
+      ox, oy = o.pos
+      maze[oy][ox] = o.to_c
     end
-
-    def _parse_maze(maze)
-#        pp maze
-        @height.times do |y|
-            @maze[y] = Array.new
-            @width.times do |x|
-#                puts "#{x},#{y}=#{maze[y][x].chr}" if $DEBUG
-                case maze[y][x].chr
-                when '#'
-                    @maze[y][x] = :wall
-                when ' '
-                    @maze[y][x] = :wall
-                when '.'
-                    @maze[y][x] = :dot
-                when '@'
-                    @maze[y][x] = :dot
-                    @objects.push PacMan.new(x, y, '@')
-                when 'V'
-                    @maze[y][x] = :dot
-                    @objects.push Enemy.new(x, y, 'V')
-                when 'H'
-                    @maze[y][x] = :dot
-                    @objects.push Enemy.new(x, y, 'H')
-                when 'L'
-                    @maze[y][x] = :dot
-                    @objects.push Enemy.new(x, y, 'L')
-                when 'R'
-                    @maze[y][x] = :dot
-                    @objects.push Enemy.new(x, y, 'R')
-                when 'J'
-                    @maze[y][x] = :dot
-                    @objects.push Enemy.new(x, y, 'J')
-                else
-                    raise
-                end
-            end
-        end
+    @height.times do |y|
+      @width.times do |x|
+        print maze[y][x]
+      end
+      puts
     end
+  end
 
-    def walk(&block)
-        @height.times do |y|
-            @width.times do |x|
-#                puts "WALKING(#{y},#{x}) ==========>" if $DEBUG
-                block.call(x,y)
-            end
-        end
-    end
-
-
-    def out_of_range?(x, y)
-#        puts "OOR:#{x},#{y}" if $DEBUG
-        if x < 0 or (x >= @width)
-#            puts "x is #{x} but width is #{@width}" if $DEBUG
-            return :x_oor
-        end
-        if y < 0 or (y >= @height)
-#            puts "y is #{y} but height is #{@height}" if $DEBUG
-            return :y_oor
-        end
-        false
-    end
-    alias oor? out_of_range?
-
-    def cell(x, y)
-        @maze[y][x]
-    end
-
-    def is(x,y)
-#        puts "IS?:#{x},#{y}" if $DEBUG
-        return nil if oor?(x,y)
-        cell(x,y)
-    end
-
-    def wall?(x,y)
-        # Out of range is as same as wall.
-        if oor?(x,y)
-            return false
-        end
-        # check the cell is wall or not
-#        puts "WALL?:#{x},#{y}=#{is(x,y)}" if $DEBUG
-        if is(x,y) == :wall
-            return :wall
-        end
-        false
-    end
-
-    def dot?(x,y)
-        is(x,y) == :dot
-    end
-    
-    def eated?(x,y)
-        is(x,y) == :eated
-    end
-
-    def enemy?(x,y)
-        @objects.each do |o|
-            ox, oy = o.pos
-            if ox == x && oy == y then
-                return true
-            end
-        end
-        false
-    end
-
-    def cango?(now, vec)
-        ox, oy = now
-        vx, vy = vec
-        gx = ox + vx
-        gy = oy + vy
-
-        # Range check
-        return false if out_of_range?(ox, oy)
-        return false if out_of_range?(gx, gy)
-        # Wall
-        return false if wall?(gx, gy)
-        # Enemy is there?
-        return false if enemy?(gx, gy)
-        # Otherwise
-        return true
-    end
-
-    def ways(pos)
-        x, y = pos
-        if oor?(x, y)
-            return nil
+  def _parse_maze(maze)
+#    pp maze
+    @height.times do |y|
+      @maze[y] = Array.new
+      @width.times do |x|
+#        puts "#{x},#{y}=#{maze[y][x].chr}" if $DEBUG
+        case maze[y][x].chr
+        when '#'
+          @maze[y][x] = :wall
+        when ' '
+          @maze[y][x] = :wall
+        when '.'
+          @maze[y][x] = :dot
+        when '@'
+          @objects.push PacMan.new(x, y, '@')
+        when 'V'
+          @objects.push Enemy.new(x, y, 'V')
+        when 'H'
+          @objects.push Enemy.new(x, y, 'H')
+        when 'L'
+          @objects.push Enemy.new(x, y, 'L')
+        when 'R'
+          @objects.push Enemy.new(x, y, 'R')
+        when 'J'
+          @objects.push Enemy.new(x, y, 'J')
         else
-            return [
-                cango?([x,y],[ 0,-1]),  # UP
-                cango?([x,y],[ 0, 1]),  # DOWN
-                cango?([x,y],[-1, 0]),  # LEFT
-                cango?([x,y],[ 1, 0])   # RIGHT
-            ]
+          raise
         end
+      end
     end
+  end
 
-    def _number_of_ways(numways, pos)
-        return nil unless dot?(pos[0], pos[1])
-        around = ways(pos)
-        return false if around.nil?
-        count = 0
-        around.map {|p| count+=1 if p}
-        count >= numways ? around : nil
+  def walk(&block)
+    @height.times do |y|
+      @width.times do |x|
+#        puts "WALKING(#{y},#{x}) ==========>" if $DEBUG
+        block.call(x,y)
+      end
     end
+  end
 
-    def cross?(pos)
-        _number_of_ways(3, pos)
-    end
-    def road?(pos)
-        _number_of_ways(2, pos)
-    end
 
-    def parse_crosses
-        walk {|x,y|
-            a = cross?([x,y])
-            if a then
-#                print "CROSS(#{x},#{y},#{is(x,y)})" ; pp a
-                @crosses.push [x,y]
-            end
-        }
+  def out_of_range?(x, y)
+    #        puts "OOR:#{x},#{y}" if $DEBUG
+    if x < 0 or (x >= @width)
+#      puts "x is #{x} but width is #{@width}" if $DEBUG
+      return :x_oor
     end
-
-    def parse_roads
-        walk {|x,y|
-            a = road?([x,y])
-            if a then
-#                print "ROAD(#{x},#{y},#{is(x,y)})" ; pp a
-                @roads.push [x,y]
-            end
-        }
+    if y < 0 or (y >= @height)
+#      puts "y is #{y} but height is #{@height}" if $DEBUG
+      return :y_oor
     end
+    false
+  end
+  alias oor? out_of_range?
 
-    attr_reader :time, :maze, :width, :height, :crosses, :roads
+  def cell(x, y)
+    @maze[y][x]
+  end
+
+  def is(x,y)
+#    puts "IS?:#{x},#{y}" if $DEBUG
+    return nil if oor?(x,y)
+    cell(x,y)
+  end
+
+  def wall?(x,y)
+    # Out of range is as same as wall.
+    if oor?(x,y)
+      return false
+    end
+    # check the cell is wall or not
+#    puts "WALL?:#{x},#{y}=#{is(x,y)}" if $DEBUG
+    if is(x,y) == :wall
+      return :wall
+    end
+    false
+  end
+
+  def dot?(x,y)
+    is(x,y) == :dot
+  end
+
+  def eated?(x,y)
+    is(x,y) == :eated
+  end
+
+  def enemy?(x,y)
+    @objects.each do |o|
+      ox, oy = o.pos
+      if ox == x && oy == y then
+        return true
+      end
+    end
+    false
+  end
+
+  def cango?(now, vec)
+    ox, oy = now
+    vx, vy = vec
+    gx = ox + vx
+    gy = oy + vy
+
+    # Range check
+    return false if out_of_range?(ox, oy)
+    return false if out_of_range?(gx, gy)
+    # Wall
+    return false if wall?(gx, gy)
+    # Enemy is there?
+    return false if enemy?(gx, gy)
+    # Otherwise
+    return true
+  end
+
+  def ways(pos)
+    x, y = pos
+    if oor?(x, y)
+      return nil
+    else
+      return [
+        cango?([x,y],[ 0,-1]),  # UP
+        cango?([x,y],[ 0, 1]),  # DOWN
+        cango?([x,y],[-1, 0]),  # LEFT
+        cango?([x,y],[ 1, 0])   # RIGHT
+      ]
+    end
+  end
+
+  def _number_of_ways(numways, pos)
+    return nil unless dot?(pos[0], pos[1])
+    around = ways(pos)
+    return false if around.nil?
+    count = 0
+    around.map {|p| count+=1 if p}
+    count >= numways ? around : nil
+  end
+
+  def cross?(pos)
+    _number_of_ways(3, pos)
+  end
+  def road?(pos)
+    _number_of_ways(2, pos)
+  end
+
+  def parse_crosses
+    walk {|x,y|
+      a = cross?([x,y])
+      if a then
+#        print "CROSS(#{x},#{y},#{is(x,y)})" ; pp a
+        @crosses.push [x,y]
+      end
+    }
+  end
+
+  def parse_roads
+    walk {|x,y|
+      a = road?([x,y])
+      if a then
+#        print "ROAD(#{x},#{y},#{is(x,y)})" ; pp a
+        @roads.push [x,y]
+      end
+    }
+  end
+
+  attr_reader :time, :maze, :width, :height, :crosses, :roads
 end
 
 #__END__
@@ -315,6 +311,6 @@ mz.parse_crosses
 puts "crosses:#{mz.crosses.size}"
 mz.parse_roads
 puts  "roads:#{mz.roads.size}"
-pp mz
+#pp mz
 #mz.go(['h', 'j', 'k', 'l'])
 
